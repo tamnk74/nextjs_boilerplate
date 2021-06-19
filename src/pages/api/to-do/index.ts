a0f954f141873faa282c5a78c665d660ae69ceb5
khactam94 <khac.tam.94@gmail.com>
2021-06-19 23:44:41 +0700

    bugfix: fix to do api

--- src/pages/api/to-do/index.ts
+++ src/pages/api/to-do/index.ts
@@ -10,23 +10,6 @@ export default (_: NextApiRequest, res: NextApiResponse<TodoRes>): void => {
   res.statusCode = 200;
   res.json({
     name: 'TO-DO LIST',
-    data: [
-      {
-        name: 'Leaning Next Js',
-        status: 1
-      },
-      {
-        name: 'Leaning Nest Js',
-        status: 2
-      },
-      {
-        name: 'Leaning CI-CD',
-        status: 0
-      },
-      {
-        name: 'Leaning Testing',
-        status: 0
-      }
-    ]
+    data: []
   });
 };
